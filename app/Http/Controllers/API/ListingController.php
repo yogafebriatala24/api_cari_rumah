37a35de4ff9f33148fae91a03f5809aeaaed99c3
Yoga Febriatala <yogafebriatala24@gmail.com>
2024-07-17 11:53:45 +0700

    update end point listings

--- app/Http/Controllers/API/ListingController.php
+++ app/Http/Controllers/API/ListingController.php
@@ -10,12 +10,20 @@ use Illuminate\Http\Request;
 class ListingController extends Controller
 {
     public function index() : JsonResponse {
-        $listings = listing::withCount('transaction')->orderBy('transaction_count', 'desc');
+        $listings = listing::withCount('transaction')->orderBy('transaction_count', 'desc')->paginate();
 
         return response()->json([
             'success' => true,
             'message' => 'Get all listings',
-            'data' => $listings
+            'data' => $listings,
+        ]);
+    }
+
+    function show(listing $listing) : JsonResponse {
+        return response()->json([
+            'success' => true,
+            'message' => 'Get detail listing',
+            'data' => $listing
         ]);
     }
 }
